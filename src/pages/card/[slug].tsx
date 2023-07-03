98a465c5f0e626b400d391bbffe023559559c710
Samuel Pires <samuel.ferpires@gmail.com>
2023-07-03 19:33:29 -0300

    feat: card info page

--- src/pages/card/[slug].tsx
+++ src/pages/card/[slug].tsx
@@ -1,6 +1,8 @@
 import { Card } from "@/types/card";
+import { Box, Flex, Grid, Loader, Text, Title } from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function CardInfoPage() {
@@ -20,9 +22,31 @@ export default function CardInfoPage() {
     staleTime: 1000 * 60 * 60 * 24,
   });
 
+  if (results.isLoading) {
+    return <Loader />;
+  }
+
   return (
-    <div>
-      <pre>{JSON.stringify(results.data, null, 2)}</pre>
-    </div>
+    <Grid grow>
+      <Grid.Col span={4}>
+        <Flex justify={"center"}>
+          <Image
+            src={results.data?.image || ""}
+            alt={`Image for ${results.data?.name}`}
+            width={350}
+            height={400}
+          />
+        </Flex>
+      </Grid.Col>
+      <Grid.Col span={8}>
+        <Flex direction={"column"} gap={16} w={"100%"}>
+          <Title order={1}>{results.data?.name}</Title>
+          <Text>{results.data?.flavorText}</Text>
+          <Flex wrap={"wrap"} gap={16}>
+            <Text>Mana cost: {results.data?.manaCost}</Text>
+          </Flex>
+        </Flex>
+      </Grid.Col>
+    </Grid>
   );
 }

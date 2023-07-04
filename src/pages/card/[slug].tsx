24a79502ab8b1ededc17dcec4dbad2678ce65df1
Samuel Pires <samuel.ferpires@gmail.com>
2023-07-04 18:42:09 -0300

    WIP: improving single card page UI

--- src/pages/card/[slug].tsx
+++ src/pages/card/[slug].tsx
@@ -1,12 +1,31 @@
 import { Card } from "@/types/card";
-import { Box, Flex, Grid, Loader, Text, Title } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Group,
+  Loader,
+  Text,
+  Title,
+  createStyles,
+} from "@mantine/core";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
+const useStyles = createStyles((theme) => ({
+  infoBox: {
+    padding: `4px ${theme.spacing.lg}`,
+    backgroundColor: theme.colors.blue[7],
+    borderRadius: "5px",
+  },
+}));
+
 export default function CardInfoPage() {
   const router = useRouter();
+  const { classes } = useStyles();
 
   const results = useQuery<Card>({
     queryKey: ["getSingleCard"],
@@ -27,26 +46,42 @@ export default function CardInfoPage() {
   }
 
   return (
-    <Grid grow>
-      <Grid.Col span={4}>
-        <Flex justify={"center"}>
-          <Image
-            src={results.data?.image || ""}
-            alt={`Image for ${results.data?.name}`}
-            width={350}
-            height={400}
-          />
+    <Flex p={3} w={"100%"} align={"center"}>
+      <Box w={"300px"} h={"400px"} pos={"relative"}>
+        <Image
+          src={results.data?.image || ""}
+          alt={`Image for ${results.data?.name}`}
+          fill
+        />
+      </Box>
+      <Flex direction={"column"} gap={10}>
+        <Title order={1}>{results.data?.name}</Title>
+        <Text fs={"italic"} size={20}>
+          {results.data?.flavorText}
+        </Text>
+        <Flex w={"100%"} justify={"start"} gap={10}>
+          <Group className={classes.infoBox}>
+            <Text weight={"bold"}>Manacost:</Text>
+            <Text>{results.data?.manaCost}</Text>
+          </Group>
+          <Group className={classes.infoBox}>
+            <Text weight={"bold"}>Atack:</Text>
+            <Text>{results.data?.attack || 0}</Text>
+          </Group>
+          <Group className={classes.infoBox}>
+            <Text weight={"bold"}>Health:</Text>
+            <Text>{results.data?.health || 0}</Text>
+          </Group>
+          <Group className={classes.infoBox}>
+            <Text weight={"bold"}>Type:</Text>
+            <Text>{results.data?.typeId || 0}</Text>
+          </Group>
         </Flex>
-      </Grid.Col>
-      <Grid.Col span={8}>
-        <Flex direction={"column"} gap={16} w={"100%"}>
-          <Title order={1}>{results.data?.name}</Title>
-          <Text>{results.data?.flavorText}</Text>
-          <Flex wrap={"wrap"} gap={16}>
-            <Text>Mana cost: {results.data?.manaCost}</Text>
-          </Flex>
+        <Flex mt={20} gap={10}>
+          <Button>Add to favorites</Button>
+          <Button>See decks</Button>
         </Flex>
-      </Grid.Col>
-    </Grid>
+      </Flex>
+    </Flex>
   );
 }

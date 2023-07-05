0ad722e7b525cb0e877ec9ab614d39a9cebbcecf
Samuel Pires <samuel.ferpires@gmail.com>
2023-07-05 18:40:31 -0300

    UI: refactoring card info component

--- src/components/CardInfo.tsx
+++ src/components/CardInfo.tsx
@@ -1,7 +1,28 @@
 import { Card as CardType } from "@/types/card";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 export default function CardInfo({ card }: { card: CardType }) {
-  return <div>{card.name}</div>;
+  return (
+    <div className="flex items-center">
+      <div className="w-40 h-40 relative">
+        <Image src={card.image} alt={`Image for ${card.name}`} fill />
+      </div>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>{card.name}</CardTitle>
+          <CardDescription>{card.flavorText}</CardDescription>
+        </CardHeader>
+        <CardContent>{card.text}</CardContent>
+      </Card>
+    </div>
+  );
 }

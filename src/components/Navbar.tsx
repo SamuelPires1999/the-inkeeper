520178faeb68e4747f8229c45b23bb39b8d30e0b
Samuel Pires <samuel.ferpires@gmail.com>
2023-07-07 22:40:43 -0300

    AUTH: implementing google auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,25 @@ import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import { X } from "lucide-react";
+import { signIn, useSession } from "next-auth/react";
 
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { data: session, status } = useSession();
 
   return (
     <>
       <div className="flex w-full p-3 lg:mb-10">
         <span className="text-4xl font-bold italic grow">The inkeeper</span>
-        <div className="lg:flex gap-3 hidden">
+        <div className="lg:flex gap-3 hidden items-center">
           <Button variant={"outline"}>Explore decks</Button>
           <Button variant={"outline"}>My decks</Button>
           <Button variant={"outline"}>Deck builder</Button>
+          {status === "unauthenticated" ? (
+            <Button onClick={() => signIn("google")}>Login With Google</Button>
+          ) : (
+            <span>Logged as {session?.user?.name}</span>
+          )}
         </div>
         <Button
           variant={"outline"}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,12 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const queryClient = new QueryClient();
 
   return (
@@ -17,12 +21,14 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
 
-      <QueryClientProvider client={queryClient}>
-        <div className="container mx-auto max-w-5xl">
-          <NavBar />
-          <Component {...pageProps} />
-        </div>
-      </QueryClientProvider>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <div className="container mx-auto max-w-5xl">
+            <NavBar />
+            <Component {...pageProps} />
+          </div>
+        </QueryClientProvider>
+      </SessionProvider>
     </>
   );
 }
